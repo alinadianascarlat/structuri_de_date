fef7e9d65f8c79401935f6d069579da3d023cf5a
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-03-23 17:35:21 +0200

    Verifica daca un numar este par

---
+++ VerificaDacaUnNumarEsteParSauImpar/VerificaDacaUnNumarEsteParSauImpar.cpp
@@ -0,0 +1,24 @@
+//39.Verififcati daca un numar este par sau impar.
+#include<iostream>
+
+using namespace std;
+
+
+int este_par(int b) {
+	if (b % 2 == 0)
+		return 1;
+	else
+		return 0;
+}
+
+int main() {
+	int a;
+	
+	cout << "a = ";
+	cin >> a;
+
+	if (este_par(a) == 1)
+		cout << "numarul este par";
+	else
+		cout << "Numarul nu este par";
+}
