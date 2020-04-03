1adf393c763ff1310a13f8b33c379e3626dd0414
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-04-03 18:02:18 +0300

    Problema permutari numere impare pe pozitii fixe

---
+++ 9.PermutariImparePastreazaPozitia/9.PermutariImparePastreazaPozitia.cpp
@@ -0,0 +1,40 @@
+﻿//9. Se citește un număr natural n.Afișați permutările mulțimii 1, 2, 3, ..., n în 
+//care elementele impare sunt puncte fixe(se află pe poziție egale cu valoarea lor)
+
+#include <iostream>
+using namespace std;
+
+int n, sol[20], p[20];
+
+void afiseaza() {
+	for (int i = 1; i <= n; i++)
+		if (i % 2 != 0)
+			cout << i << " ";
+		else
+			cout << sol[i] << " ";
+	cout << endl;
+}
+
+void backtracking(int k)
+{
+	for (int i = 2; i <= n; i = i + 2)
+		if (p[i] == 0)
+		{
+			sol[k] = i;
+			p[i] = 1;
+
+			if ((n % 2 == 1 && k == n - 1) || (n % 2 == 0 && k == n))
+				afiseaza();
+			else
+				backtracking(k + 2);
+			p[i] = 0;
+		}
+}
+
+int main()
+{
+	cout << "n = ";
+	cin >> n;
+	cout << "Permutari:" << endl;
+	backtracking(2);
+}
---
+++ CombinatiiBinareCareNuAu2cifre1/CombinatiiBinareCareNuAu2cifre1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int v[20], n;
+
+void afisare() {
+	for (int i = 0; i <= n; i++)
+		cout << v[i];
+	cout << endl;
+}
+void backtracking(int k) {
+	for (int i = 0; i <= 1; i++) {
+		v[k] = i;
+		if (k > 1)
+			if (v[k] * v[k - 1] != 1)
+				if (k == n)
+					afisare();
+				else
+					backtracking(k + 1);
+			else;
+		else  backtracking(k + 1);
+	}
+}
+int main() {
+	cout << "n = ";
+	cin >> n;
+	backtracking(1);
+	return 0;
+}
